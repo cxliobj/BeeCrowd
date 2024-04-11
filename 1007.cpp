5d4d531d2fe342c3c4121abe3095c874e7e186f1
Celio <cxliobj@outlook.com>
2024-04-11 14:43:26 -0300

    Alguns problemas a mais

---
+++ 1007.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int numA, numB, numC, numD;
+    std::cin >> numA >> numB >> numC >> numD;
+
+    std::cout << "DIFERENCA = " << numA*numB - numC*numD << std::endl;
+
+    return 0;
+}
